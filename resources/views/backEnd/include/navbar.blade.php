2b8389a00919ee6adb06e4fc7c486f05f9cbbafd
Md.Shariful islam <sharif.net888@gmail.com>
2024-05-10 12:37:42 -0700

    Estimates Complete

--- resources/views/backEnd/include/navbar.blade.php
+++ resources/views/backEnd/include/navbar.blade.php
@@ -37,11 +37,6 @@
           <i class="fas fa-expand-arrows-alt"></i>
         </a>
       </li>
-      {{-- <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li> --}}
       <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" href="#">
           <i class="fas fa-th-large"></i>
--- resources/views/backEnd/include/script.blade.php
+++ resources/views/backEnd/include/script.blade.php
@@ -8,7 +8,8 @@
 </script>
 
 <!-- Bootstrap 4 -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script><!-- ChartJS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="{{ asset('backEndAsset') }}/plugins/chart.js/Chart.min.js"></script>
 <!-- Sparkline -->
 <script src="{{ asset('backEndAsset') }}/plugins/sparklines/sparkline.js"></script>
