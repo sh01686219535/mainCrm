3c455eaa509a82d250c3ee1e7c01165fcf2de68f
Md.Shariful islam <sharif.net888@gmail.com>
2024-05-11 10:19:25 -0700

    Estimates Complete

--- resources/views/backEnd/include/script.blade.php
+++ resources/views/backEnd/include/script.blade.php
@@ -8,7 +8,6 @@
 </script>
 
 <!-- Bootstrap 4 -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="{{ asset('backEndAsset') }}/plugins/chart.js/Chart.min.js"></script>
 <!-- Sparkline -->
